276505984a8c85406940bc482f26c704ffa62a8a
habale <lhbang95@gmail.com>
2016-11-21 21:48:34 +0700

    Add files via upload
    # Change slideshow javascript

--- src/js/slideshow.js
+++ src/js/slideshow.js
@@ -1,63 +1,53 @@
 var myIndex = 0;
 carousel();
 
-// function indexIncrease(n){
-//   myIndex+=n-1;
-//     for (i = 0; i < x.length; i++) {
-//        x[i].style.display = "none";
-//        y[i].style.display = "none";
-//        z[i].style.display = "none";
-//        t[i].style.display = "none";
-//        cir[i].style.backgroundColor ="white";
-//     }
-//     x[myIndex-1].style.display = "block";
-//     y[myIndex-1].style.display = "inline";
-//     z[myIndex-1].style.display = "inline"; 
-//     t[myIndex-1].style.display = "block"; 
-//     cir[myIndex-1].style.backgroundColor = "rgba(122,157,150,1)";
-// }
+function leftarrow(){
+  var llitem = document.getElementById("left-left-item");
+  var litem = document.getElementById("left-item");
+  var citem = document.getElementById("center-item");
+  var ritem = document.getElementById("right-item");
+  var rritem = document.getElementById("right-right-item");
+  // llitem.style.transition="none";
+  llitem.id = "right-right-item";
+  litem.id = "left-left-item";
+  citem.id = "left-item";
+  ritem.id = "center-item";
+  rritem.id = "right-item";
+}
 
+function rightarrow(){
+  var llitem = document.getElementById("left-left-item");
+  var litem = document.getElementById("left-item");
+  var citem = document.getElementById("center-item");
+  var ritem = document.getElementById("right-item");
+  var rritem = document.getElementById("right-right-item");
 
-// function currentDiv(n) {
-// 	myIndex=n-1;
-//     for (i = 0; i < x.length; i++) {
-//        x[i].style.display = "none";
-//        y[i].style.display = "none";
-//        z[i].style.display = "none";
-//        t[i].style.display = "none";
-//        cir[i].style.backgroundColor ="white";
-//     }
-//     x[myIndex-1].style.display = "block";
-//     y[myIndex-1].style.display = "inline";
-//     z[myIndex-1].style.display = "inline"; 
-//     t[myIndex-1].style.display = "block"; 
-//     cir[myIndex-1].style.backgroundColor = "rgba(122,157,150,1)";
-// }
+  var item = document.getElementsByClassName("slideshow-item");
+  // llitem.style.transition="none";
+  // for (i = 0; i < 5; i++) {item[i].style.transition = "all .2s ease-in-out";}
+  llitem.id = "left-item";
+  litem.id = "center-item";
+  citem.id = "right-item";
+  ritem.id = "right-right-item";
+  rritem.id = "left-left-item";
+}
 
 function carousel() {
+    // var item = document.getElementsByClassName("slideshow-item");
+    // for (i = 0; i < 5; i++) {item[i].classList.remove("slow-transition");}
+    leftarrow();
+    // for (i = 0; i < 5; i++) {item[i].classList.add("slow-transition");}
+
     var i;
-    var x = document.getElementsByClassName("image-slideitem");
-    var y = document.getElementsByClassName("infor1-slideitem");
-    var z = document.getElementsByClassName("infor2-slideitem");
-    var t = document.getElementsByClassName("header-slideitem");
     var cir = document.getElementsByClassName("circle-slideitem");
-    
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";
-       y[i].style.display = "none";
-       z[i].style.display = "none";
-       t[i].style.display = "none";
+    for (i = 0; i < 5; i++) {
        cir[i].style.backgroundColor ="white";
     }
 
     myIndex++;
-    if (myIndex > x.length) {myIndex = 1;}
+    if (myIndex > 5) {myIndex = 1;}
 
-    x[myIndex-1].style.display = "block";
-  	y[myIndex-1].style.display = "inline";
-  	z[myIndex-1].style.display = "inline"; 
-  	t[myIndex-1].style.display = "block"; 
-  	cir[myIndex-1].style.backgroundColor = "rgba(122,157,150,1)";
+    cir[myIndex-1].style.backgroundColor = "rgba(122,157,150,1)";
 
-    setTimeout(carousel, 3000);
+    setTimeout(carousel, 5000);
 }
