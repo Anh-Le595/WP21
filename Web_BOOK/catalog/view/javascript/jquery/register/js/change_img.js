cd52ce6e68ff204c3adf538943c775edbaf7ae28
Anh_Le95 <51300080@hcmut.edu.vn>
2017-05-11 21:05:48 +0800

    Update new version of Web_BOOK

--- Web_BOOK/catalog/view/javascript/jquery/register/js/change_img.js
+++
@@ -1,6 +0,0 @@
-window.money_format = "{{amount_no_decimals_with_comma_separator}} VND";
-         var enable_lazy_loading = true,
-          enable_ajax_cart = true,
-          change_image = false;
-          
-         change_image = true;
